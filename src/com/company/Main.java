aa52a0a12499e3ae37559918f25cef28d5ebb9a4
ha271097 <nguyenquangha2710@gmail.com>
2021-04-05 14:13:35 +0700

    4/5/21

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,12 @@ public class Main {
         System.out.println("number 1: Print the rectangle");
         System.out.println("number 2: Print the square triangle (The corner is square at 4 different angles: top-left, top-right, botton-left, botton-right)");
         System.out.println("number 3: Print isosceles triangle");
+        System.out.println("number 4: Tam giac can");
         System.out.println("number 0: Exit");
         int number = sc.nextInt();
         switch (number){
             case 1:
-                for (int i =0; i<4 ; i++ ){
+                for (int i =0; i<3 ; i++ ){
                     for(int j = 0; j<7 ; j++){
                         System.out.print("*");
                     }
@@ -35,6 +36,19 @@ public class Main {
                         System.out.print("*");
                     }
                     System.out.println("");
+                }
+                break;
+            case 4:
+                for (int i =1 ; i <= 5 ; i++){
+                    for (int j =1 ; j <= 5*2 ; j++ ){
+                        if((Math.abs(5 - j)) <= (i - 1)){
+                            System.out.print("*");
+                        }
+                        else {
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
                 }
                 break;
             case 0:
